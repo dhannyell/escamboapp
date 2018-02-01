199994454c43708229723e08e026457d4bb508fb
dhannyell <dhannyelllukas@hotmail.com>
2018-02-01 15:03:22 -0300

    cookies

--- app/controllers/site/home_controller.rb
+++ app/controllers/site/home_controller.rb
@@ -1,8 +1,10 @@
 class Site::HomeController < SiteController
 
   def index
+  
     @categories = Category.order(:description)
     @ads = Ad.descending_order(params[:page]) #Scope
     @carousel = Ad.random_carousel(3) #Scope
+
   end
 end
--- app/controllers/site/search_controller.rb
+++ app/controllers/site/search_controller.rb
@@ -1,5 +1,7 @@
 class Site::SearchController < SiteController
     def ads
+        cookies[:search_term] = params[:q]
+        
         @categories = Category.order(:description)
         @ads = Ad.search(params[:q], params[:page])
     end
