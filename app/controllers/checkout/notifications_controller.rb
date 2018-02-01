aca38a7e23ae2ba13e5797357d81e2afd1213c59
dhannyell <dhannyelllukas@hotmail.com>
2018-02-01 01:19:11 -0300

    Ajustando pagamento com o pagseguro

--- app/controllers/checkout/notifications_controller.rb
+++ app/controllers/checkout/notifications_controller.rb
@@ -2,23 +2,22 @@ class Checkout::NotificationsController < ApplicationController
     skip_before_filter :verify_authenticity_token, only: :create
   
     def create
-      transaction = PagSeguro::Transaction.find_by_notification_code(params[:notificationCode])
+        transaction = PagSeguro::Transaction.find_by_notification_code(params[:notificationCode])
   
-      if transaction.errors.empty?
-        status_code = transaction.status.id.to_i
-        order_id = transaction.reference.to_i
+        if transaction.errors.empty?
+            status_code = transaction.status.id.to_i
+            order_id = transaction.reference.to_i
   
-        order = Order.find(order_id)
-        order.status = status_code
-        order.save
+            order = Order.find(order_id)
+            order.status = status_code
+            order.save
   
-        if status_code == 3
-          ad = Ad.find(order.ad.id)
-          ad.sold!
+            if status_code == 3
+                ad = Ad.find(order.ad.id)
+                ad.sold!
+            end
         end
-      end
   
-      render nothing: true, status: 200
+        render nothing: true, status: 200
     end
-  end
-  
+end
